9477a80898d673b18146f873e5d2a975d9fbd81c
Alex Borsuk <alex.borsuk@dh.com>
2017-04-28 13:18:14 -0400

    changing config to sandbox

--- src/configurations.ts
+++ src/configurations.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    // public ApiAuthServer: string = "https://sandbox.desinto.com/";
-    // public ApiServer: string = "https://sandbox.desinto.com/";
-    public ApiAuthServer: string = "http://localhost:51840/";
-    public ApiServer: string = "http://localhost:51840/";
+    public ApiAuthServer: string = "https://sandbox.desinto.com/";
+    public ApiServer: string = "https://sandbox.desinto.com/";
+    // public ApiAuthServer: string = "http://localhost:51840/";
+    // public ApiServer: string = "http://localhost:51840/";
     public ApiAuthUrl: string = "customerregistrationapi/login";
     public ApiAuthLogoutUrl: string = "customerregistrationapi/logout";
     public ApiCampaignsUrl: string = "customercampaignsapi/getcampaigns";
