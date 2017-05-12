f8b42c58fa61c93f593bc0a5e3bbcc02f9c7415a
Alex Borsuk <alex.borsuk@dh.com>
2017-05-12 11:50:50 -0400

    minor code review changes

--- src/pages/campaign/campaign.ts
+++ src/pages/campaign/campaign.ts
@@ -51,20 +51,15 @@ export class CampaignPage implements OnInit {
 
     var markCampaingAsVisitedRequestModel = new MarkCampaingAsVisitedRequest(token, this.campaign.Id);
     this.dataServiceGateway.post("customercampaignsapi/markcustomercampaignvisited", markCampaingAsVisitedRequestModel).subscribe(
-      (response) => {
-        /* this function is executed every time there's a new output */
-        console.log("VALUE RECEIVED: " + response);
+      (response) => {        
         this.campaignViewed = true;
-        campaign.IsViewed = true;
-        // TODO: Here we should reload campaign and update view
+        campaign.IsViewed = true;        
       },
-      (err) => {
-        /* this function is executed when there's an ERROR */
-        console.log("ERROR: " + err);
+      (err) => {        
+        
       },
-      () => {
-        /* this function is executed when the observable ends (completes) its stream */
-        console.log("COMPLETED");
+      () => {        
+        
       });
   }
 
