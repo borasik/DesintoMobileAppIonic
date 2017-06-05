e52761ba31612224dccf295e45520d85685aa64a
Alex Borsuk <ab5645@dhltd.corp>
2017-06-05 18:58:02 -0400

    Adding wait spinner to Campaign list screen

--- src/pages/campaigns/campaigns.ts
+++ src/pages/campaigns/campaigns.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Pipe } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, Loading} from 'ionic-angular';
 import { CampaignPage } from '../campaign/campaign';
 import { DataServiceGateway } from '../../services/data-gateway-service';
 import { CustomerCampaignsRequest } from '../../models/customer-campaigns-request';
@@ -7,25 +7,25 @@ import { LoginPage } from '../login/login';
 import { Configuration } from "../../configurations";
 
 @Component({
-  selector: 'page-campaigns', 
+  selector: 'page-campaigns',
   templateUrl: 'campaigns.html'
 })
 export class CampaignsPage {
   icons: string[];
+  loading: Loading;
   campaigns: object[];
-  thumpUrlPrefix : any;
+  thumpUrlPrefix: any;
   private loginPage = LoginPage;
 
   constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams, 
+    public navCtrl: NavController,
+     private loadingCtrl: LoadingController,
+    public navParams: NavParams,
     private dataServiceGateway: DataServiceGateway,
     private configurations: Configuration
-    ) {
+  ) {
     this.campaigns = [];
-    this.thumpUrlPrefix = configurations.ApiServer;    
-   
-    
+    this.thumpUrlPrefix = configurations.ApiServer;
   }
 
   itemTapped(event, campaign) {
@@ -34,8 +34,8 @@ export class CampaignsPage {
     });
   }
 
-  contentType(campaign){
-    switch(campaign.ContentType){
+  contentType(campaign) {
+    switch (campaign.ContentType) {
       case 0:
         return "URL";
       case 1:
@@ -44,13 +44,14 @@ export class CampaignsPage {
         return "Downloads Rewards";
       default:
         return "N/A";
-    } 
+    }
   }
 
   ionViewWillEnter() {
+    this.showLoading();
     let token: string;
     try {
-      token = (JSON.parse(localStorage.getItem('currentUser'))).token;      
+      token = (JSON.parse(localStorage.getItem('currentUser'))).token;
     }
     catch (exception) {
       this.navCtrl.setRoot(this.loginPage);
@@ -59,17 +60,24 @@ export class CampaignsPage {
     var customerCampaignModel = new CustomerCampaignsRequest(token);
     this.dataServiceGateway.post("customercampaignsapi/getcampaigns", customerCampaignModel).subscribe(
       (response) => {
-        /* this function is executed every time there's a new output */
-        console.log("VALUE RECEIVED: " + response);
+        //this.loading.dismiss();
         this.campaigns = response.campaigns;
       },
       (err) => {
-        /* this function is executed when there's an ERROR */
+         this.loading.dismiss();
         console.log("ERROR: " + err);
       },
       () => {
-        /* this function is executed when the observable ends (completes) its stream */
+         this.loading.dismiss();
         console.log("COMPLETED");
       });
   }
+
+  showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    this.loading.present();
+  }
+
 }
